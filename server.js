3ece96bb9de4a3ca39ed1d10c0f180bb80348dc8
Mike <4xgood@gmail.com>
2020-11-22 13:01:00 -0700

    auth is setup

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
-require("dotenv").config();
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,21 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ extended: true }));
 
-const uri =
-  "mongodb+srv://mongo:mongo@cluster0.ameg5.mongodb.net/Groceries?retryWrites=true&w=majority";
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
 
 //Define Routes
 app.use("/listItems", require("./routes/listItems"));
 app.use("/users", require("./routes/users"));
+app.use("/auth", require("./routes/auth"));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
